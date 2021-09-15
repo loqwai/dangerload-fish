8ade960ab459b6c5029a25acd1731356134086b4
redaphid <iam@aaronherres.com>
2021-09-14 20:04:23 -0700

    close to hidden func

--- functions/dls.fish
+++ functions/dls.fish
@@ -20,18 +20,30 @@ function dangerload --description "dangerously sources whatever's in ./scripts/d
         set old_func_header $old_func[2]
         set old_func_footer $old_func[-1]
         set old_func_body $old_func[3..-1]
+        
+
+        set old_func_header_pieces (string split ' ' $old_func_header)
+
+        set  -la hidden_func_header $old_func_header_pieces[1]
+        functions --erase $hidden_func_header
+        set -la hidden_func_header '_'$old_func_header_pieces[2]
+        set -la hidden_func_header $old_func_header_pieces[3..-1]
+        
+        echo 'hidden func header' $hidden_func_header
 
         set -e new_func
-        set -a new_func '_'$old_func_header';'
-        set -a new_func $old_func        
-        set -a new_func ';dangerload;'
+        set -a new_func $old_func_header';'
+
+        set -a new_func $hidden_func_header 
+        set -a new_func ';'
         set -a new_func $old_func_body';'
+        set -a new_func ';dangerload;'
         set -a new_func _$n \$argv';'
         set -a new_func $old_func_footer';'
 
-        functions --erase $n
+        functions --erase $old_func_header
         set new_body (string join \n $new_func)
-        echo $new_body
+        echo $new_func
         eval $new_body
 
     end
--- tests/hello-world.fish
+++ tests/hello-world.fish
@@ -1,4 +1,4 @@
 #!/usr/bin/env fish
 function hello-world
-    echo "bye, world!"
+    echo "hi, world!"
 end
