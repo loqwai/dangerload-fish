071562ea46f5690ddfc491d45ce5ec077d0f86d4
redaphid <iam@aaronherres.com>
2021-09-14 18:30:52 -0700

    got setting and erasing down

--- functions/dls.fish
+++ functions/dls.fish
@@ -1,15 +1,23 @@
 #!/usr/bin/env fish
 function dangerload -a include_file --description "does the opposite of .gitignore; only source the files referenced in the .dangerload-include file"
     string length -q "$include_file"; or set include_file ./scripts/dangerload.fish
-    
-    set _dls_old_functions (functions)
-    set -e _dls_new_functions
+    dangerunload
+    set -e _dls_new_functions    
+    set -g _dls_old_functions (functions)
+
 
     # for i in $_dls_old_functions
     #     echo $i
     # end
     source $include_file
     for i in (functions)
-        contains $i $_dls_old_functions; or set -a _dls_new_functions $i
+        contains $i $_dls_old_functions; or set -ag _dls_new_functions $i
+    end
+    echo $_dls_new_functions
+end
+
+function dangerunload
+    for f in $_dls_new_functions
+        functions --erase $f
     end
 end
--- tests/test-hello-world.fish
+++ tests/test-hello-world.fish
@@ -1,1 +1,2 @@
+#!/usr/bin/env fish
 functions --erase hello-world; source ./functions/dls.fish; dangerload ./tests/hello-world.fish
