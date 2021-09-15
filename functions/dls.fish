f8df8210533f3bbb6e9a1d6b9ec84868b91bd5cd
redaphid <iam@aaronherres.com>
2021-09-14 18:19:00 -0700

    we can tell what functions are new. That's something. at least

--- functions/dls.fish
+++ functions/dls.fish
@@ -1,6 +1,17 @@
 #!/usr/bin/env fish
-function dangerload-include -a include_file --description "does the opposite of .gitignore; only source the files referenced in the .dangerload-include file"
-    string length -q "$include_file"; or set include_file .dangerload-include
-    echo $include_file
+function dangerload -a include_file --description "does the opposite of .gitignore; only source the files referenced in the .dangerload-include file"
+    string length -q "$include_file"; or set include_file ./scripts/dangerload.fish
+    
+    set _dls_old_functions (functions)
+    set -e _dls_new_functions
 
+    # for i in $_dls_old_functions
+    #     echo $i
+    # end
+    source $include_file
+    for i in (functions)
+        contains $i $_dls_old_functions; or set -a _dls_new_functions $i
+    end
+    echo "new functions:"
+    echo $_dls_new_functions    
 end
---
+++ tests/hello-world.fish
@@ -0,0 +1,4 @@
+#!/usr/bin/env fish
+function hello-world
+    echo "hello, world!"
+end
